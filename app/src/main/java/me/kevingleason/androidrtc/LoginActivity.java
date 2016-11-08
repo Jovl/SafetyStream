0ae0808731254fa8fc2a83fd72fb050c878c5be2
Dan Silverio <Dan Silverio>
2016-11-07 23:36:12 -0500

    test code for trying to connect to database

--- app/src/main/java/me/kevingleason/androidrtc/LoginActivity.java
+++ app/src/main/java/me/kevingleason/androidrtc/LoginActivity.java
@@ -1,6 +1,9 @@
 package me.kevingleason.androidrtc;
 
 import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,12 +13,22 @@ import me.kevingleason.androidrtc.util.Constants;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.Button;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 /**
  * Login Activity for the first time the app is opened, or when a user clicks the sign out button.
  * Saves the username in SharedPreferences.
  */
 public class LoginActivity extends Activity {
+
+    private Button Submit;
 
     private EditText FirstName;
     private EditText LastName;
@@ -28,6 +41,11 @@ public class LoginActivity extends Activity {
     private Spinner Gender;
     private EditText Age;
     private EditText mUsername;
+
+    // Declaring connection variables
+    Connection con;
+    String un,pass,db,ip;
+    //End Declaring connection variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +75,101 @@ public class LoginActivity extends Activity {
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, genders);
         dropdown.setAdapter(adapter2);
 
+        Submit = (Button) findViewById(R.id.SubmitButton);
+
+        //TODO Fill in strings with actual names
+        // Declaring Server ip, username, database name and password
+        ip = "your server ip here";
+        db = "your database name here";
+        un = "your username for that database here";
+        pass = "your password for that database here";
+        // Declaring Server ip, username, database name and password
+
+        // Setting up the function when button login is clicked
+        Submit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CheckLogin checkLogin = new CheckLogin();// this is the Asynctask, which is used to process in background to reduce load on app process
+                checkLogin.execute("");
+            }
+        });
+        //End Setting up the function when button login is clicked
+
+
+
 
     }
 
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
 
+        String fName = FirstName.getText().toString();
+        String lName = LastName.getText().toString();
+        String address = Address.getText().toString();
+        String city = City.getText().toString();
+        String zipcode = ZipCode.getText().toString();
+        String age = Age.getText().toString();
+        String state = State.getText().toString();
+        String phone = PhoneNumber.getText().toString();
+        String race = Race.getText().toString();
+        String gender = Gender.getSelectedItem().toString();
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            //TODO Add conditional statements to validate data entry boudaries
+                try
+                {
+                    con = connectionclass(un, pass, db, ip);        // Connect to database
+                    if (con == null)
+                    {
+                        z = "Check Your Internet Access!";
+                    }
+                    else
+                    {
+                        //TODO Figure out how to write to Database
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = ex.getMessage();
+                }
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return connection;
+    }
     public void testButton (View view){
         //when the app is opened for the first time the user enters all their information. After the information has been entered,
         //it is stored and their first name is used as the username and stored so that in the future the user does not need to sign
